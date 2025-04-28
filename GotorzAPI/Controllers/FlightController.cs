bd419e4c20eb9a65cb103d34bdbcbaaf1e1c4583
Fractusa <56714875+Fractusa@users.noreply.github.com>
2025-04-28 14:02:37 +0200

    Very Early API draft

---
+++ GotorzAPI/Controllers/FlightController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using GotorzAPI.Models;
+//using System.Web.Http;
+
+namespace GotorzAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FlightController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("flights/{city}")]
+        public async Task<IActionResult> GetFlights(string city)
+        {
+            string apiKey = "HHgMzxzswzCmOEQNl9Qa9nnB6JIEFeuG";
+            string apiUrl = $"test.api.amadeus.com";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode) 
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+
+                    var flights = JsonConvert.DeserializeObject<FlightData>(json);
+
+                    
+                }
+
+                return BadRequest("Error when finding flights");
+            }
+        }
+    }
+}
---
+++ GotorzAPI/Controllers/HotelController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GotorzAPI.Controllers
+{
+    public class HotelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
