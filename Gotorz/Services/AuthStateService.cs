20194f44c0c9f3ee1b8d983e9f371541fce1db70
Mathias Nielsen <mgni62378@edu.ucl.dk>
2025-05-12 03:24:44 +0200

    Made login authentication state
    Created login authentication state service to allow the login method to set the succesfully logged in user as the logged in user state. Allowing the UserPage to check whether an user is logged in or not and give access to information based on that.
    
    Need to add Roles to the authentication service to allow for specific functions to be allowed or limited to different kind of users based on their roles.

---
+++ Gotorz/Services/AuthStateService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Gotorz.Services
+{
+    public class AuthStateService
+    {
+        public bool IsLoggedIn { get; set; }
+        public string UserEmail { get; set; }
+
+        //Event to allow other component to subscribe to, allowing them to refresh when login state changes
+        public event Action? OnChange;
+
+        //Method to login the user and set the login state
+        public void Login(string email)
+        {
+            IsLoggedIn = true;
+            UserEmail = email;
+            NotifyStateChanged();
+
+        }
+        //Method to logout the user and clear the login state
+        public void Logout()
+        {
+            IsLoggedIn = false;
+            UserEmail = null;
+            NotifyStateChanged();
+        }
+
+        //Method to raise the OnChange event, when loginState changes
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
