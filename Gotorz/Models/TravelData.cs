a8c7708c64af67b04f83ea93b2d0f2344d042fd3
Fractusa <56714875+Fractusa@users.noreply.github.com>
2025-05-15 23:34:34 +0200

    Unnecessary code removed

--- Gotorz/Models/TravelData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Gotorz.Models
-{
-    public class TravelData
-    {
-        public List<FlightData> OutboundFlights { get; set; } = new();
-        public List<FlightData> ReturnFlights { get; set; } = new();
-        public List<HotelData> Hotels { get; set; } = new();
-    }
-}
