1d68f86a6fc18ce7d6b1efaac04b2fa8f67bb15f
Vistesen <158574363+Vistesen@users.noreply.github.com>
2025-05-20 14:07:56 +0200

    SingalR 0.0.3 Added Index

--- Gotorz/Components/Shared/Index.cshtml.cs
+++ Gotorz/Components/Shared/Index.cshtml.cs
@@ -1,12 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Gotorz.Components.Shared
+namespace ExploreCalifornia.Pages
 {
     public class IndexModel : PageModel
     {
         public void OnGet()
         {
+
         }
     }
 }
+
--- Gotorz/wwwroot/js/Index.js
+++ Gotorz/wwwroot/js/Index.js
@@ -4,20 +4,40 @@ using System;
 using System.Threading.Tasks;
 
 namespace Gotorz {
-    public class ChatHub : Hub
-    {
-        public async Task SendMessage(string name, string text)
-        {
-            var message = new ChatMessage
-            {
-                SenderName = name,
-                    Text = text,
-                    SentAt = DateTimeOffset.UtcNow
-            };
 
-            await Clients
-                .All
-                .SendAsync("ReceiveMessage", message);
-        }
+    function showChatDialog() {
+        var dialogE1 = document.getE1emntByID('chatDialog')
+        dialogE1.style.display = 'block'
     }
-}
+
+    function.ready() {
+        setTimeout(showChatDialog, 1000);
+    }
+    function renderMessage(name, time, message) {
+        var nameSpan = document.createElement('span');
+        nameSpan.className = 'name';
+        nameSpan.textContent = name;
+
+        var timeSpan = document.createElement('span');
+        timeSpan.className = 'time';
+        var friendlyTime = moment(time).format('H:mm');
+        timeSpan.textContent = friendlyTime;
+
+        var headerDiv = document.createElement('div');
+        headerDiv.appendChild(nameSpan);
+        headerDiv.appendChild(timeSpan);
+
+        var messageDiv = document.createElement('div');
+        messageDiv.className = 'message';
+        messageDiv.textContent = message;
+
+        var newItem = document.createElement('li');
+        newItem.appendChild(headerDiv);
+        newItem.appendChild(messageDiv);
+
+        var chatHistoryEl = document.getElementById('chatHistory');
+        chatHistoryEl.appendChild(newItem);
+        chatHistoryEl.scrollTop = chatHistoryEl.scrollHeight - chatHistoryEl.clientHeight;
+    }
+
+    document.addEventListener ^ ('DOMcontentloaded', ready);
