801fce41b1d24911de48f7457ae9da00c4a3cfa4
Fractusa <56714875+Fractusa@users.noreply.github.com>
2025-05-23 03:49:40 +0200

    Ability to create packages added
    Currently saving in-memory list, should be expanded to save and load from a database, to easier display for customers. Also need to display name of airport and hotel when displaying packages.

---
+++ Gotorz/Models/TravelPackage.cs
@@ -0,0 +1,11 @@
+﻿namespace Gotorz.Models
+{
+    public class TravelPackage
+    {
+        public FlightData DepartureFlight { get; set; }
+        public FlightData ReturnFlight { get; set; }
+        public HotelData Hotel { get; set; }
+
+        public decimal TotalPrice => DepartureFlight.Price + ReturnFlight.Price + Hotel.Price;
+    }
+}
