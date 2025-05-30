89d6a81d3ec307f0ba9124c7e80ca990ce48dc43
Mathias Nielsen <mgni62378@edu.ucl.dk>
2025-05-30 03:31:18 +0200

    Replaced orders with bookings
    Deleted orders and made the program use bookings in it's place

--- Gotorz/Data/ApplicationDbContext.cs
+++ Gotorz/Data/ApplicationDbContext.cs
@@ -9,7 +9,6 @@ namespace Gotorz.Data
 
         public DbSet<RegisterModel> Users { get; set; }
         public DbSet<Message> Messages { get; set; }
-        public DbSet<Order> Orders { get; set; }
         public DbSet<Booking> Bookings { get; set; }
         public DbSet<TravelPackageTemplate> TravelPackageTemplates { get; set; }
 
